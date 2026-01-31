49c1987dd6b51ccf9ec3cbcc0ddeebaac3b123a7
thevanshit <vanshit2509@gmail.com>
2026-02-01 01:25:58 +0530

    solve one question today

--- CP 31 Sheet/1000/B_Monsters.cpp
+++ CP 31 Sheet/1000/B_Monsters.cpp
@@ -1,9 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 void solve(){
-    
+    long long n;
+    long long k;
+    cin >> n >> k;
+
+    vector <long long> a(n);
+    for(long long &x : a){
+        cin >> x;
+        x = x % k;
+        if (!x) x = k; 
+    }
+
+    vector <long long> order(n);
+    iota(order.begin(), order.end(), 0);
+    stable_sort(order.begin(), order.end(), [&](long long x, long long y){
+        return a[x] > a[y];
+    });
+
+    for(auto &x : order) cout << x + 1 << " ";
+    cout << "\n";
 }
 
 
