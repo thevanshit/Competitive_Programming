47dd8587e3d33a292f241fadca465c5743fd472b
thevanshit <vanshit2509@gmail.com>
2026-02-05 11:49:13 +0530

    solve 2nd question of today

---
+++ CP 31 Sheet/1000/A_Add_and_Divide.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void solve(){
+	long long A, B, a, b, res, i, ans;
+	cin >> A >> B;
+	if(!A){
+		cout << 0 << endl;
+		return;
+	}
+	res = A + 3;
+	for(i = (B < 2 ? 2-B : 0); i < res; ++i){
+		b = B+i;
+		a = A;
+		ans = i;
+		while(a){
+			a /= b;
+			++ans;
+		}
+		if(ans < res)res = ans;
+	}
+	cout << res << endl;
+}
+
+int main(){
+    ios :: sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
