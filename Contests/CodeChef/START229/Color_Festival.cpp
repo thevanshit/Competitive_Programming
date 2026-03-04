6190bc2ebf5765db9bc583137f8a8323c742d30b
thevanshit <vanshit2509@gmail.com>
2026-03-04 21:28:27 +0530

    a

---
+++ Contests/CodeChef/START229/Color_Festival.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void solve(){
+    int n;
+    cin >> n;
+    unordered_set <int> st;
+    for (int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        st.insert(x);
+    }
+    cout << st.size() << "\n";
+}
+
+
+int main(){
+    ios :: sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
