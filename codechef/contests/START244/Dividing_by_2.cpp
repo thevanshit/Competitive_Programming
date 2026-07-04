e2e9a5c8d56dc22575df431d3e79ea2011bab623
thevanshit <vanshit2509@gmail.com>
2026-07-05 04:27:37 +0530

    feat: add AtCoder ABC-464 and CodeChef START244 solutions

---
+++ codechef/contests/START244/Dividing_by_2.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr)
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define repr(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define endl '\n'
+
+const int MOD = 1'000'000'007;
+const int INF = 1'000'000'000;
+const ll LINF = 1'000'000'000'000'000'000LL;
+const ld EPS = 1e-9;
+const ld PI = acos(-1.0);
+
+ll mod_add(ll a, ll b, ll m = MOD) { return ((a % m) + (b % m)) % m; }
+ll mod_sub(ll a, ll b, ll m = MOD) { return ((a % m) - (b % m) + m) % m; }
+ll mod_mul(ll a, ll b, ll m = MOD) { return ((a % m) * (b % m)) % m; }
+ll mod_pow(ll a, ll b, ll m = MOD) {
+    ll res = 1;
+    a %= m;
+    while (b > 0) {
+        if (b & 1) res = (res * a) % m;
+        a = (a * a) % m;
+        b >>= 1;
+    }
+    return res;
+}
+ll mod_inv(ll a, ll m = MOD) { return mod_pow(a, m - 2, m); }
+
+bool is_prime(ll n) {
+    if (n < 2) return false;
+    if (n < 4) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (ll i = 5; i * i <= n; i += 6)
+        if (n % i == 0 || n % (i + 2) == 0) return false;
+    return true;
+}
+
+vector<ll> get_divisors(ll n) {
+    vector<ll> res;
+    for (ll i = 1; i * i <= n; ++i) {
+        if (n % i == 0) {
+            res.pb(i);
+            if (i != n / i) res.pb(n / i);
+        }
+    }
+    sort(all(res));
+    return res;
+}
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+void solve() {
+    int n;
+    cin >> n;
+    vll a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    unordered_map<ll, pair<ll, int>> mp;
+
+    for (ll x : a) {
+        ll val = x;
+        int steps = 0;
+        while (val > 0) {
+            mp[val].first += steps;
+            mp[val].second++;
+            val /= 2;
+            steps++;
+        }
+        // Also include 0
+        mp[0].first += steps;
+        mp[0].second++;
+    }
+
+    ll ans = LINF;
+    for (auto &[val, data] : mp) {
+        if (data.second == n) {
+            ans = min(ans, data.first);
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    FAST_IO;
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
