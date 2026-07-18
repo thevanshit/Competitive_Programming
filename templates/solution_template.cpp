f7ef0c305771d589b999a09568e406508ce5d697
thevanshit <vanshit2509@gmail.com>
2026-07-18 22:20:34 +0530

    Replace heavy CP template with minimal candidate-master style boilerplate
    - Reduced to essentials: bits/stdc++.h, ll/vi/pii, all/sz/pb, solve() + main()
    - Removed: mod math, number theory, combinatorics, Fenwick/SegTree/DSU, debug, I/O helpers, grid helpers, string algos, binary search template

--- templates/solution_template.cpp
+++ templates/solution_template.cpp
@@ -2,38 +2,22 @@
 using namespace std;
 
 using ll = long long;
-using ld = long double;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
 using vi = vector<int>;
-using vll = vector<ll>;
-using vvi = vector<vi>;
-template<typename T> using pq = priority_queue<T>;
-template<typename T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+using pii = pair<int, int>;
 
 #define all(x) begin(x), end(x)
-#define rall(x) rbegin(x), rend(x)
 #define sz(x) (int)(x).size()
 #define pb push_back
-#define fi first
-#define se second
-
-const int MOD = 1'000'000'007;
-const ll LINF = 1'000'000'000'000'000'000LL;
-const ld PI = acos(-1.0);
-
-ll mod_pow(ll a, ll b, ll m = MOD) {
-    ll res = 1; a %= m;
-    while (b) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1; }
-    return res;
-}
 
 void solve() {
 
 }
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(nullptr);
-    int t = 1; cin >> t;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
     while (t--) solve();
 }
