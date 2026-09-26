f481c61f072c0488d792d9b4867e197c22342201
thevanshit <vanshit2509@gmail.com>
2026-09-26 22:51:15 +0530

    a

---
+++ codeforces/B_M_Stars.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using pii = pair<int, int>;
+
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    while (t--) solve();
+}
